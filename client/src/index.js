18fb8d81af39df0a99dc19c86af4176e477e60c8
Tyler Kiesman <tylerkiesman@gmail.com>
2020-06-17 20:07:15 -0400

    Updated client to ask backend for list of stocks

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var http = require('http');
+
+http.get('http://localhost:8080', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(data);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
