18d6ba9b1eda98787fd9f5ca61d7c8a865ac5d07
Tyler Kiesman <tylerkiesman@gmail.com>
2020-07-24 23:33:01 -0400

    Got rid of original React comment

--- client/src/index.js
+++ client/src/index.js
@@ -47,7 +47,4 @@ http.get('http://localhost:8080', (resp) => {
   console.log("Error: " + err.message);
 });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
